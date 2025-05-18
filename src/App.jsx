886943e624675c056075eec348d7c31e9ba0f541
Andrea-Sgura <andreasgura96@gmail.com>
2025-05-19 01:53:00 +0200

    [UPD] Creata struttura base + ciclato array con metodo map

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
 import { useState } from 'react'
 
+const listMovies = [
+  { title: 'Inception', genre: 'Fantascienza' },
+  { title: 'Il Padrino', genre: 'Thriller' },
+  { title: 'Titanic', genre: 'Romantico' },
+  { title: 'Batman', genre: 'Azione' },
+  { title: 'Interstellar', genre: 'Fantascienza' },
+  { title: 'Pulp Fiction', genre: 'Thriller' },
+]
 
 function App() {
 
   return (
     <>
-      <h1>Hello World</h1>
+      <div className="container">
+        <div className="row gy-4">
+          <div className="col-12">Movies</div>
+          <div className="col-12">
+            <ul className="list-group">
+              {listMovies.map((movie, index) => {
+                return (
+                  <li key={index} className="list-group-item">
+                    {movie.title} - <em>{movie.genre}</em>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
